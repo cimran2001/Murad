dd6d6a15af13f422ab35dbd1158c27223d90d009
c.imran2001 <c.imran2001@yandex.com>
2022-03-12 19:20:27 +0400

    Homework Task 1 Solution added

--- Lesson10/Homework/sets.py
+++ Lesson10/Homework/sets.py
@@ -1,3 +1,17 @@
 U = {i for i in range(2, 101)}
 
 A = {i for i in U if i % 2 == 0}
+B = {i for i in U if i % 3 == 0}
+C = {i for i in U if i % 5 == 0 or i % 7 == 0}
+
+a = A | B
+b = A | C
+c = B - C
+d = A ^ B
+e =  b | B
+
+print(f"\t{a=}")
+print(f"\t{b=}")
+print(f"\t{c=}")
+print(f"\t{d=}")
+print(f"\t{e=}")
